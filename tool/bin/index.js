e47166e7980f09e46efc5240e844e8a5f9e374b2
Eshan Patel <cancelledcancer@gmail.com>
2023-09-02 21:53:42 +0530

    added basic error handling

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -2,15 +2,24 @@
 
 import arg from 'arg';
 
-const args = arg({
-  '--start': Boolean,
-  '--build': Boolean,
-});
+try {
+  const args = arg({
+    '--start': Boolean,
+    '--build': Boolean,
+  });
 
-if (args['--start']) {
-  console.log('starting the tool');
+  if (args['--start']) {
+    console.log('starting the tool');
+  }
+} catch (e) {
+  console.log(e.message);
+  usage();
 }
 
 // console.log(args);
 
-function helloWorld() {}
+function usage() {
+  console.log(`tool [CMD]
+  --start\tStarts the app
+  --build\tBuilds the app`);
+}
